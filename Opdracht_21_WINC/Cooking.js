e9647463b3334ae07715188ced952e31863bb97c
AlexanderMetselaar <80575076+AlexanderMetselaar@users.noreply.github.com>
2021-06-15 11:58:25 +0200

    Add files via upload

---
+++ Opdracht_21_WINC/Cooking.js
@@ -0,0 +1,19 @@
+const makingPizza = function () {
+    console.log("Making dough for the Pizza");
+    console.log("Creating the Pizza form");
+    console.log("Putting on all the ingredients for the Pizza");
+    console.log("Bake the pizza in the oven");
+}
+
+const makingSushi = function () {
+    console.log("Boil rice, and cool it down");
+    console.log("Mix all the ingredients together");
+    console.log("Roll the rice and ingredients in a sheet of seaweed");
+    console.log("Cut the role into pieces and enjoy!");
+}
+
+makingSushi();
+makingSushi();
+makingSushi();
+makingPizza();
+makingPizza();
